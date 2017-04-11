5ec282dafd50e53750c7e594b80053a6e18e11ca
Wataru.Kasahara <kasahara1019@gmail.com>
2017-04-11 22:32:55 +0900

    Example: Todo List using Redux
    ref. http://redux.js.org/docs/basics/ExampleTodoList.html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import todoApp from './reducers'
+
+let store = createStore(todoApp)
 
 ReactDOM.render(
-  <div>Hello, world!</div>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
