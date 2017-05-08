ba9388d7d1d99ed54a047ecb34ac77f27684cf0a
Wataru.Kasahara <kasahara1019@gmail.com>
2017-05-08 09:50:50 +0900

    新規 container の作成

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import Main from './containers/Main'
+import App from './containers/App'
 import todoApp from './reducers'
 
 let store = createStore(todoApp)
 
 ReactDOM.render(
   <Provider store={store}>
-    <Main />
+    <App />
   </Provider>,
   document.getElementById('root')
 )
