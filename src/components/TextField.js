6e667dc1b4d2f0ef96002df689a364b961f7b24f
Wataru.Kasahara <kasahara1019@gmail.com>
2017-05-31 21:59:33 +0900

    テキストボックスに文字を入力できるように修正

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -3,7 +3,16 @@ import React from 'react';
 export default class TextField extends React.Component {
   render() {
     return (
-      <input type="text" />
+      <input
+        type="text"
+        value={this.props.editingText}
+        onChange={e => this.props.onChange(e.target.value)}
+      />
     );
   }
 }
+
+TextField.propTypes = {
+  editingText: React.Props.string.isRequired,
+  onChange: React.Props.func.isRequired,
+};
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { inputText } from '../actions';
+
 import Button from '../components/Button';
 import TextField from '../components/TextField';
 
 class App extends React.Component {
   render() {
+    const { dispatch } = this.props;
+    const onChange = (text) => {
+      dispatch(inputText(text));
+    };
+
     return (
       <div>
-        <TextField />
+        <TextField
+          value={this.props.editingText}
+          onChange={onChange}
+        />
         <Button />
       </div>
     );
   }
 }
 
+App.propTypes = {
+  editingText: React.Props.string.isRequired,
+};
 
 export default connect()(App);
